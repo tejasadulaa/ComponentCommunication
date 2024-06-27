5caf322cae173952aafb90fa4e8d351851f33c62
tejasadulaa <teja.sadulaa@gmail.com>
2024-06-27 16:11:51 -0700

    Add project files.

---
+++ Data/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace ComponentCommunication_1165395.Data
+{
+    public class UserInfo
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
